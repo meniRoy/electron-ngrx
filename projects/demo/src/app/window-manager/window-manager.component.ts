ef273741555b17a30adf46b22db93f7660bf09c0
Alexeys Valners <avalner@gmail.com>
2021-06-01 16:03:30 +0300

    chore: angular update to version 9.1.13

--- projects/demo/src/app/window-manager/window-manager.component.ts
+++ projects/demo/src/app/window-manager/window-manager.component.ts
@@ -1,28 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {map} from 'rxjs/operators';
-import {ActivatedRoute, UrlSegment} from '@angular/router';
-import {Observable} from 'rxjs';
-import {ElectronService} from '../providers/electron.service';
+import { Component, OnInit } from "@angular/core";
+import { map } from "rxjs/operators";
+import { ActivatedRoute, UrlSegment } from "@angular/router";
+import { Observable } from "rxjs";
+import { ElectronService } from "../providers/electron.service";
 
 @Component({
-  selector: 'window-manager',
-  templateUrl: './window-manager.component.html',
-  styleUrls: ['./window-manager.component.scss']
+  selector: "window-manager",
+  templateUrl: "./window-manager.component.html",
+  styleUrls: ["./window-manager.component.scss"],
 })
 export class WindowManagerComponent implements OnInit {
-  private winId: number;
-  private windowRoute$: Observable<string>;
+  winId: number;
+  windowRoute$: Observable<string>;
 
   constructor(
     private electronService: ElectronService,
-    private activatedRoute: ActivatedRoute) {
-  }
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.winId = this.electronService.remote.getCurrentWindow().id;
     this.windowRoute$ = this.activatedRoute.url.pipe(
-      map((urlSegments: UrlSegment[]) => '/' + urlSegments.map((urlSegment: UrlSegment) => urlSegment.path).join('/'))
+      map(
+        (urlSegments: UrlSegment[]) =>
+          "/" +
+          urlSegments.map((urlSegment: UrlSegment) => urlSegment.path).join("/")
+      )
     );
   }
-
 }
