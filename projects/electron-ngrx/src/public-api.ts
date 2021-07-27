494b97a815bc3aa741db78e63f66256e2964f20f
Eliyahu Basa <eliyahub@wix.com>
2021-07-27 17:36:23 +0300

    Update public-api.ts

--- projects/electron-ngrx/src/public-api.ts
+++ projects/electron-ngrx/src/public-api.ts
@@ -1,4 +1,1 @@
-export * from './lib/electron-ngrx.service';
-export * from './lib/window-communication.service';
-export * from './lib/electron-ngrx.module';
-export {registerSelector} from './lib/selector-manager';
+
